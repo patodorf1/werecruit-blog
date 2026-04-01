9f55132e2567db88af933e7128d54170d6dc27f1
Patricio Dussel <pato@Patricios-MacBook-Pro.local>
2026-04-01 19:04:22 -0300

    Add homepage replicating werecruit.it, restructure blog to /blog

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -15,7 +15,7 @@ export async function GET(context: APIContext) {
       title: post.data.title,
       pubDate: post.data.date,
       description: post.data.description,
-      link: `/${post.id}/`,
+      link: `/blog/${post.id}/`,
     })),
   });
 }
